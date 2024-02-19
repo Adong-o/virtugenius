8199b184df59a90700fabc73fd30a3b895e61fcf
Adongo Jakes <116814115+Adong-o@users.noreply.github.com>
2024-02-19 10:55:15 +0300

    Virtu-Genius

---
+++ main.js
@@ -0,0 +1,19 @@
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    document.querySelector('.nav-links').classList.toggle('active');
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.menu-toggle').addEventListener('click', function() {
+      document.querySelector('.nav-links').classList.toggle('active');
+    });
+  });
+  
+
+document.querySelector('.call-button').addEventListener('click', function() {
+    window.location.href = 'tel:+254757689592';
+  });
+  
+
+
+  
+  
